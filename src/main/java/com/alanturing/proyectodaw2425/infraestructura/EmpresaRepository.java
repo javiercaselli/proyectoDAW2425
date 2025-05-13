bde683cfd760011291d118dcdcd69bb170e58f15
Javier Caselli <7252806+javiercaselli@users.noreply.github.com>
2025-05-13 17:22:57 +0200

    CRUD Institutos hasta tabla de listado

---
+++ src/main/java/com/alanturing/proyectodaw2425/infraestructura/EmpresaRepository.java
@@ -0,0 +1,17 @@
+package com.alanturing.proyectodaw2425.infraestructura;
+
+import com.alanturing.proyectodaw2425.dominio.Empresa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EmpresaRepository extends JpaRepository<Empresa, String> {
+
+    @Query("SELECT em FROM Empresa em WHERE em.email = :email AND em.contrasenia = :password")
+    public Optional<Empresa> findByEmailAndPassword(@Param("email") String email, @Param("password") String password);
+
+}
--- src/main/java/com/alanturing/proyectodaw2425/infraestructura/InstitutoRepository.java
+++ src/main/java/com/alanturing/proyectodaw2425/infraestructura/InstitutoRepository.java
@@ -2,8 +2,16 @@ package com.alanturing.proyectodaw2425.infraestructura;
 
 import com.alanturing.proyectodaw2425.dominio.Instituto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface InstitutoRepository extends JpaRepository<Instituto, Long> {
+
+    @Query("SELECT i FROM Instituto i WHERE i.email = :email AND i.contrasenia = :password")
+    public Optional<Instituto> findByEmailAndPassword(@Param("email") String email, @Param("password") String password);
+
 }
